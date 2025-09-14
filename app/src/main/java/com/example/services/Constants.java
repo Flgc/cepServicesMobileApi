dcaa3bc483444bc6f0c2b22ccb92f151e14ec71c
Fábio Luís <ml@fabiotecnico1.biz>
2025-09-14 13:38:46 -0300

    Setting up Rout/Endpoint in the Project

--- app/src/main/java/com/example/services/Constants.java
+++ app/src/main/java/com/example/services/Constants.java
@@ -1,4 +1,6 @@
 package com.example.services;
 
 public class Constants {
+    public static String URL = "https://api.invertexto.com";
+    public static String TOKEN = "@#$@%$";
 }
--- app/src/main/java/com/example/services/api/InverTextoApi.java
+++ app/src/main/java/com/example/services/api/InverTextoApi.java
@@ -1,4 +1,17 @@
 package com.example.services.api;
 
-public class InverTextoApi {
+import com.example.services.model.Address;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+/* Retrofit turns your HTTP API into a Java (or Kotlin) interface. */
+public interface InverTextoApi {
+    @GET("/v1/cep/{zipCodNumber}")
+    Call<Address> getAddress(
+            @Path("zipCodNumber") String zipCodNumber,
+            @Query("token") String token
+    );
 }
